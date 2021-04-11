70c9e963a0d3bc6324f011b17dc1319639d66aec
yuval-ya <yakovksind@gmail.com>
2021-04-11 18:30:17 +0300

    graph and algorithm types

---
+++ shortest-path-problem/FordBaseAlgorithm.h
@@ -0,0 +1,9 @@
+//
+//  FordBaseAlgorithm.h
+//  shortest-path-problem
+//
+//  Created by Yuval  Yakovskind  on 11/04/2021.
+//  Copyright © 2021 Yuval Yakovskind. All rights reserved.
+//
+
+#pragma once
